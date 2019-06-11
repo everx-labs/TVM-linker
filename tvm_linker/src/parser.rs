bc1c9e3e1d1e938983078e8f5612afd54dec1190
Nikita.m <monakhovnik@gmail.com>
2019-06-12 02:29:03 +0300

    parsing of global and persistent data into dicts

--- tvm_linker/src/parser.rs
+++ tvm_linker/src/parser.rs
@@ -12,18 +12,19 @@ use ton_block::*;
 enum ObjectType {
     None,
     Function((u32, String)),
-    Data((u64, Vec<DataValue>)),
+    Data { addr: u64, values: Vec<DataValue>, persistent: bool },
 }
 impl From<&str> for ObjectType {
     fn from(stype: &str) -> ObjectType {
         match stype {
             "function" => ObjectType::Function((0, String::new())),
-            "object" => ObjectType::Data((0, vec![])),
+            "object" => ObjectType::Data{ addr: 0, values: vec![], persistent: false },
             _ => ObjectType::None,
         }
     }
 }
 enum DataValue {
+    Empty,
     Number((IntegerData, usize)),
 }
 
@@ -38,6 +39,7 @@ impl DataValue {
                 b.append_bitstring(&dest_vec[..]).unwrap();
                 b
             },
+            DataValue::Empty => b,
         }
     }
 }
@@ -74,8 +76,10 @@ pub struct ParseEngine {
     internals: HashMap<i32, String>,
     signed: HashMap<u32, bool>,
     entry_point: String,
-    next_obj: usize,
-    data_pointer: u64
+    globl_base: u64,
+    globl_ptr: u64,
+    persistent_base: u64,
+    persistent_ptr: u64,
 }
 
 const PATTERN_GLOBL:    &'static str = r"^[\t\s]*\.globl[\t\s]+([a-zA-Z0-9_]+)";
@@ -83,6 +87,8 @@ const PATTERN_DATA:     &'static str = r"^[\t\s]*\.data";
 const PATTERN_INTERNAL: &'static str = r"^[\t\s]*\.internal[\t\s]+(:[a-zA-Z0-9_]+)";
 const PATTERN_SELECTOR: &'static str = r"^[\t\s]*\.selector";
 const PATTERN_ALIAS:    &'static str = r"^[\t\s]*\.internal-alias (:[a-zA-Z0-9_]+),[\t\s]+(-?\d+)";
+const PATTERN_GLBLBASE: &'static str = r"^[\t\s]*\.global-base[\t\s]+([0-9]+)";
+const PATTERN_PERSBASE: &'static str = r"^[\t\s]*\.persistent-base[\t\s]+([0-9]+)";
 const PATTERN_LABEL:    &'static str = r"^[\.a-zA-Z0-9_]+:";
 const PATTERN_PARAM:    &'static str = r"^[\t\s]+\.([a-zA-Z0-9_]+),?[\t\s]*([a-zA-Z0-9_]+)";
 const PATTERN_TYPE:     &'static str = r"^[\t\s]*\.type[\t\s]+([a-zA-Z0-9_]+),[\t\s]*@([a-zA-Z]+)";
@@ -93,10 +99,18 @@ const INTERNAL: &'static str = ".internal";
 const DATA:     &'static str = ".data";
 const SELECTOR: &'static str = ".selector";
 
+//const FUNCTION_TYPENAME:&'static str = "function";
+const DATA_TYPENAME:    &'static str = "object";
+
 const FUNC_SUFFIX_AUTH: &'static str = "_authorized";
+const PERSISTENT_DATA_SUFFIX: &'static str = "_persistent";
+
+const PUBKEY_NAME: &'static str = "tvm_public_key";
+const SCI_NAME: &'static str = "tvm_contract_info";
 
 const OFFSET_NEXT_DATA: u64 = 8;
-const DATA_BASE_POINTER: u64 = 8000;
+const OFFSET_GLOBL_DATA: u64 = 8;
+const OFFSET_PERS_DATA: u64 = 16;
 
 impl ParseEngine {
 
@@ -109,12 +123,16 @@ impl ParseEngine {
             internals:  HashMap::new(),
             signed:     HashMap::new(),
             entry_point: String::new(),
-            next_obj:   0,
-            data_pointer: DATA_BASE_POINTER,
+            globl_base: 0,
+            globl_ptr: 0,
+            persistent_base: 0,
+            persistent_ptr: 0,
         }
     }
 
     pub fn parse<T: Read + Seek>(&mut self, source: T, libs: Vec<T>) -> Result<(), String> {
+        self.preinit()?;
+
         for lib_buf in libs {
             let mut reader = BufReader::new(lib_buf);
             self.parse_code(&mut reader, true)?;
@@ -135,10 +153,7 @@ impl ParseEngine {
     }
 
     pub fn data(&self) -> BuilderData {
-        let mut data = BuilderData::new();
-        let cell = self.build_data().cell();
-        data.append_reference(BuilderData::from(&cell));
-        data
+        BuilderData::from_slice(&self.build_data())
     }
 
     pub fn entry(&self) -> &str {
@@ -182,6 +197,43 @@ impl ParseEngine {
         &self.signed
     }
 
+    fn preinit(&mut self) -> Result <(), String> {
+        self.globals.insert(
+            PUBKEY_NAME.to_string(), 
+            Object::new(PUBKEY_NAME.to_string(), DATA_TYPENAME)
+        );
+        match self.globals.get_mut(PUBKEY_NAME).unwrap().dtype {
+            ObjectType::Data {addr: _, ref mut values, ref mut persistent} => {
+                *persistent = true;
+                values.push(DataValue::Empty);
+            },
+            _ => ()
+        };
+
+        self.globals.insert(
+            SCI_NAME.to_string(), 
+            Object::new(SCI_NAME.to_string(), DATA_TYPENAME)
+        );
+        match self.globals.get_mut(SCI_NAME).unwrap().dtype {
+            ObjectType::Data {addr: _, ref mut values, ref mut persistent} => {
+                *persistent = false;
+                values.push(DataValue::Empty);
+            },
+            _ => ()
+        };
+        ok!()
+    }
+
+    fn update_predefined(&mut self) {
+        if let ObjectType::Data { ref mut addr, values: _, persistent: _ } = self.globals.get_mut(SCI_NAME).unwrap().dtype {
+            *addr = self.globl_base;
+        }
+
+        if let ObjectType::Data { ref mut addr, values: _, persistent: _ } = self.globals.get_mut(PUBKEY_NAME).unwrap().dtype {
+            *addr = self.persistent_base;
+        }
+    }
+
     pub fn parse_code<R: BufRead>(&mut self, reader: &mut R, first_pass: bool) -> Result<(), String> {
         let globl_regex = Regex::new(PATTERN_GLOBL).unwrap();
         let internal_regex = Regex::new(PATTERN_INTERNAL).unwrap();
@@ -192,29 +244,53 @@ impl ParseEngine {
         let alias_regex = Regex::new(PATTERN_ALIAS).unwrap();
         let type_regex = Regex::new(PATTERN_TYPE).unwrap();
         let size_regex = Regex::new(PATTERN_SIZE).unwrap();
+        let base_glbl_regex = Regex::new(PATTERN_GLBLBASE).unwrap();
+        let base_pers_regex = Regex::new(PATTERN_PERSBASE).unwrap();
 
         let mut section_name: String = String::new();
         let mut obj_body: String = "".to_owned();
         let mut obj_name: String = "".to_owned();
         let mut lnum = 0;
         let mut l = String::new();
-        self.data_pointer = DATA_BASE_POINTER;
+              
+
         while reader.read_line(&mut l)
             .map_err(|_| "error while reading line")? != 0 {
             lnum += 1;
-            if type_regex.is_match(&l) {
+            if base_glbl_regex.is_match(&l) {
+                // .global-base
+                let cap = base_glbl_regex.captures(&l).unwrap();
+                let base = cap.get(1).map(|m| m.as_str())
+                    .ok_or(format!("line {}: invalid syntax for global base", lnum))?;
+                self.globl_base = u64::from_str_radix(base, 10)
+                    .map_err(|_| format!("line {}: invalid global base address", lnum))?;
+                self.globl_ptr = self.globl_base + OFFSET_GLOBL_DATA;
+                self.update_predefined();
+            } else if base_pers_regex.is_match(&l) {
+                // .persistent-base
+                let cap = base_pers_regex.captures(&l).unwrap();
+                let base = cap.get(1).map(|m| m.as_str())
+                    .ok_or(format!("line {}: invalid syntax for persistent base", lnum))?;
+                self.persistent_base = u64::from_str_radix(base, 10)
+                    .map_err(|_| format!("line {}: invalid persistent base address", lnum))?;
+                self.persistent_ptr = self.persistent_base + OFFSET_PERS_DATA;
+                self.update_predefined();
+            } else if type_regex.is_match(&l) {
+                // .type x, @...
                 let cap = type_regex.captures(&l).unwrap();
                 let name = cap.get(1).unwrap().as_str().to_owned();
                 let type_name = cap.get(2).ok_or(format!("line:{}: .type option is invalid", lnum))?.as_str();
                 let obj = self.globals.entry(name.clone()).or_insert(Object::new(name, &type_name));
                 obj.dtype = ObjectType::from(type_name);
             } else if size_regex.is_match(&l) {
+                // .size x, val
                 let cap = size_regex.captures(&l).unwrap();
                 let name = cap.get(1).unwrap().as_str().to_owned();
                 let size_str = cap.get(2).ok_or(format!("line:{}: .size option is invalid", lnum))?.as_str();
                 let item_ref = self.globals.entry(name.clone()).or_insert(Object::new(name, ""));
                 item_ref.size = usize::from_str_radix(size_str, 10).unwrap_or(0);
             } else if globl_regex.is_match(&l) { 
+                // .globl x
                 self.update(&section_name, &obj_name, &obj_body, first_pass)
                     .map_err(|e| format!("line {}: {}", lnum, e))?;
                 section_name = GLOBL.to_owned();
@@ -222,12 +298,14 @@ impl ParseEngine {
                 obj_name = globl_regex.captures(&l).unwrap().get(1).unwrap().as_str().to_owned();
                 self.globals.entry(obj_name.clone()).or_insert(Object::new(obj_name.clone(), ""));
             } else if data_regex.is_match(&l) {
+                // .data
                 self.update(&section_name, &obj_name, &obj_body, first_pass)
                     .map_err(|e| format!("line {}: {}", lnum, e))?;
                 section_name = DATA.to_owned();
                 obj_name = "".to_owned();
                 obj_body = "".to_owned();
             } else if selector_regex.is_match(&l) {                
+                // .selector
                 self.update(&section_name, &obj_name, &obj_body, first_pass)?;
                 if first_pass { 
                     section_name.clear();
@@ -237,12 +315,14 @@ impl ParseEngine {
                 obj_name = "".to_owned();
                 obj_body = "".to_owned();
             } else if internal_regex.is_match(&l) {
+                // .internal
                 self.update(&section_name, &obj_name, &obj_body, first_pass)
                     .map_err(|e| format!("line {}: {}", lnum, e))?;
                 section_name = INTERNAL.to_owned();
                 obj_body = "".to_owned();
                 obj_name = internal_regex.captures(&l).unwrap().get(1).unwrap().as_str().to_owned();
             } else if alias_regex.is_match(&l) {
+                // .internal-alias
                 let cap = alias_regex.captures(&l).unwrap();
                 self.aliases.insert(
                     cap.get(1).unwrap().as_str().to_owned(), 
@@ -250,8 +330,9 @@ impl ParseEngine {
                         .map_err(|_| format!("line: '{}': failed to parse id", lnum))?, 
                 );                
             } else if label_regex.is_match(&l) { 
-                
+                //TODO: for goto
             } else if dotted_regex.is_match(&l) {
+                // .param [value]
                 let cap = dotted_regex.captures(&l).unwrap();
                 let param = cap.get(1).unwrap().as_str();
                 match param {
@@ -280,10 +361,8 @@ impl ParseEngine {
             },
             GLOBL => {
                let item = self.globals.get_mut(func).unwrap();
-               item.index = self.next_obj;
-               self.next_obj += 1;
-                match item.dtype {
-                    ObjectType::Function(ref mut fparams) => {
+                match &mut item.dtype {
+                    ObjectType::Function(fparams) => {
                         let mut signed = false;
                         if let Some(index) = func.find(FUNC_SUFFIX_AUTH) {
                             if (index + FUNC_SUFFIX_AUTH.len()) == func.len() {
@@ -299,10 +378,19 @@ impl ParseEngine {
                             Err(format!("global function with id = {} already exist", func_id))?;
                         }
                     },
-                    ObjectType::Data(ref mut dparams) => {
-                        Self::update_data(body, func, &mut item.size, &mut dparams.1)?;
-                        dparams.0 = self.data_pointer;
-                        self.data_pointer += (dparams.1.len() as u64) * OFFSET_NEXT_DATA;
+                    ObjectType::Data { addr, ref mut values, persistent } => {
+                        if func.ends_with(PERSISTENT_DATA_SUFFIX) {
+                            *persistent = true;
+                        }
+                        Self::update_data(body, func, &mut item.size, values)?;
+                        let offset = (values.len() as u64) * OFFSET_NEXT_DATA;
+                        if *persistent { 
+                            *addr = self.persistent_ptr;
+                            self.persistent_ptr += offset;
+                        } else { 
+                            *addr = self.globl_ptr;
+                            self.globl_ptr += offset;
+                        }
                     },
                     ObjectType::None => Err(format!("The type of global object {} is unknown. Use: .type {}, xxx", func, func))?,
                 };
@@ -357,24 +445,37 @@ impl ParseEngine {
     }
 
     fn build_data(&self) -> SliceData {
-        let mut dict = HashmapE::with_bit_len(64);
-        let data_vec: Vec<(u64, &Vec<DataValue>)> = 
-            self.globals.iter().filter_map(|item| {
-                match &item.1.dtype {
-                    ObjectType::Data(ref values) => Some((values.0, &values.1)),
-                    _ => None,
-                }
-            })
-            .collect();
-
-        for item in data_vec {
-            let mut ptr = item.0;
-            for subitem in item.1 {
-                dict.set(ptr.write_to_new_cell().unwrap().into(), subitem.write().into()).unwrap();
-                ptr += OFFSET_NEXT_DATA;
+        let filter = |is_persistent: bool| self.globals.iter().filter_map(move |item| {
+            match &item.1.dtype {
+                ObjectType::Data { addr, values, persistent } => 
+                    if *persistent == is_persistent { Some((addr, values)) } else { None },
+                _ => None,
             }
-        }
-        dict.get_data()
+        });
+        let globl_data_vec: Vec<(&u64, &Vec<DataValue>)> = filter(false).collect();
+        let pers_data_vec: Vec<(&u64, &Vec<DataValue>)> = filter(true).collect();
+
+        let build_dict = |data_vec: &Vec<(&u64, &Vec<DataValue>)>| {
+            let mut dict = HashmapE::with_bit_len(64);
+            for item in data_vec {
+                let mut ptr = item.0.clone();
+                for subitem in item.1 {
+                    dict.set(ptr.write_to_new_cell().unwrap().into(), subitem.write().into()).unwrap();
+                    ptr += OFFSET_NEXT_DATA;
+                }
+            }
+            dict
+        };
+        
+        let globl_dict = build_dict(&globl_data_vec);
+        let mut pers_dict = build_dict(&pers_data_vec);
+
+        pers_dict.set(
+            (self.persistent_base + 8).write_to_new_cell().unwrap().into(), 
+            globl_dict.get_data(),
+        ).unwrap();
+
+        pers_dict.get_data()
     }
 
     fn replace_labels(&mut self, line: &str) -> String {
@@ -382,7 +483,7 @@ impl ParseEngine {
         .or_else(|_| resolve_name(line, |name| self.intrefs.get(name).map(|id| id.clone())))
         .or_else(|_| resolve_name(line, |name| self.globals.get(name).and_then(|obj| {
                 match &obj.dtype {
-                    ObjectType::Data(values) => Some(values.0),
+                    ObjectType::Data { addr, values: _, persistent: _ } => Some(addr),
                     _ => None,
                 }           
             })))
@@ -489,7 +590,7 @@ mod tests {
 
             DROP DROP
         ", 
-        base = DATA_BASE_POINTER,
+        base = OFFSET_GLOBL_DATA,
         offset = OFFSET_NEXT_DATA,
         ))
         .with_stack(
