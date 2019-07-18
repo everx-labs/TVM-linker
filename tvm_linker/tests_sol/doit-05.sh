ffc3e4c876aef99d421dd7a830a59fa81eb7bbbf
Victor Bargachev <victorb@tonlabs.io>
2019-07-18 14:18:22 +0300

    Updated test_suite.py and doit-05.sh for a new linker

--- tvm_linker/tests_sol/doit-05.sh
+++ tvm_linker/tests_sol/doit-05.sh
@@ -1,15 +1,15 @@
-address1=30d2f11fc097ddc8abb7f81dcd9279c763e42ed28c2741f8f49a1ade2e055d36
-msginit1=30d2f11f-msg-init.boc
-msgbody1=30d2f11f-msg-body.boc
-address2=bbafe9ae5e8eb876e5444f856fecb237d531ca628170e2616ce5d2200318974b
-msginit2=bbafe9ae-msg-init.boc
+address1=03d796589e3f02cf70c707743dbeb0074edb1e583ba245448d0f75fc846130be
+msginit1=03d79658-msg-init.boc
+msgbody1=03d79658-msg-body.boc
+address2=0f2dccd895dfa554a407381dbcc61dff27e1c4a92172f984eb9f127df61c2780
+msginit2=0f2dccd8-msg-init.boc
 
 rm -f *.tvc *.boc *.tmp
 
 source set_env.sh
 
-$linker --lib ../stdlib_sol.tvm ./contract05-a.code --abi-json ./contract05-a.abi.json
-$linker --lib ../stdlib_sol.tvm ./contract05-b.code --abi-json ./contract05-b.abi.json
+$linker compile --lib ../stdlib_sol.tvm ./contract05-a.code --abi-json ./contract05-a.abi.json
+$linker compile --lib ../stdlib_sol.tvm ./contract05-b.code --abi-json ./contract05-b.abi.json
 
 if [ ! -f "${address1}.tvc" ]; then
   echo "FILE NOT FOUND! ${address1}.tvc"
@@ -21,10 +21,10 @@ if [ ! -f "${address2}.tvc" ]; then
   exit 1
 fi
 
-$linker $address1 message --init -w 0
-$linker $address2 message --init -w 0
+$linker message $address1 --init -w 0
+$linker message $address2 --init -w 0
 
-$linker $address1 message -w 0 --abi-json contract05-a.abi.json --abi-method method_external \
+$linker message $address1 -w 0 --abi-json contract05-a.abi.json --abi-method method_external \
 	--abi-params "{\"anotherContract\":\"0x${address2}\", \"x\":\"257\"}"
 
 zeroes=0000000000000000000000000000000000000000000000000000000000000000
