410ed97defed6f5ba024e9ba4ca871ebf546fe9e
IgorKoval <igorkoval999@gmail.com>
2019-07-24 13:51:33 +0300

    Add comments for contract02-{a,b}.sol

--- tvm_linker/tests_sol/contract02-a.sol
+++ tvm_linker/tests_sol/contract02-a.sol
@@ -1,18 +1,21 @@
 pragma solidity ^0.5.0;
 
+// the interface of a remove contract
 contract AnotherContract {
-	function remoteMethod(uint64 value) pure public;
+    function remoteMethod(uint64 value) pure public;
 }
 
+// the contract calling the remote method
 contract MyContract {
 
-	uint m_counter;
+    // persistent variable storing the number of function 'method' was called
+    uint m_counter;
 
-	function method(AnotherContract anotherContract) public {
-		// call remote contract
-		anotherContract.remoteMethod(257);
-		m_counter = m_counter + 1;
-		return;
-	}
-	
+    function method(AnotherContract anotherContract) public {
+        // call function of remote contract
+        anotherContract.remoteMethod(257);
+        m_counter = m_counter + 1; // incrementing the counter
+        return;
+    }
+
 }
