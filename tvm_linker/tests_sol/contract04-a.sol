4c2b294ba8ab1c384729f8d68a91e4275781b8f0
IgorKoval <igorkoval999@gmail.com>
2019-07-24 11:35:00 +0300

    Using a distinct interface instead of dummy function for a remote contract

--- tvm_linker/tests_sol/contract04-a.sol
+++ tvm_linker/tests_sol/contract04-a.sol
@@ -1,21 +1,18 @@
 pragma solidity ^0.5.0;
 
+contract AnotherContract {
+        function remoteMethod(uint value) pure public;
+}
+
 contract MyContract {
 
 	uint m_counter;
 
-	function method(MyContract anotherContract, uint amount) public {
+        function method(AnotherContract anotherContract, uint amount) public {
 		// call remote contract
 		anotherContract.remoteMethod(amount);
 		m_counter = m_counter + 1;
 		return;
-	}
-	
-	// Dummy function for a remote contract. Later to be represented by a distinct interface
-	
-	function remoteMethod(uint value) pure public {
-		require(value > 0);
-		return; 
-	}
-	
+        }
+
 }
