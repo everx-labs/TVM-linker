3ef5663fff3ca46abaa22f904310b5005ed3b8a7
Gorelyshev Sergey <49919055+SeHor05@users.noreply.github.com>
2019-08-26 09:47:24 +0300

    Fix#2

--- prerequesites.sh
+++ prerequesites.sh
@@ -1,9 +1,11 @@
-apk add --no-cache make gcc linux-headers bsd-compat-headers gawk bison binutils coreutils diffutils gettext bash grep sed texinfo perl
+apk add perl
 wget https://ftp.gnu.org/gnu/glibc/glibc-2.28.tar.gz
 tar -xzf glibc-2.28.tar.gz
 cd glibc-2.28
 mkdir glibc-build
 cd glibc-build
-../configure --prefix=/usr\--disable-profile --enable-add-ons\--libexecdir=/usr/bin --with-headers=/usr/include\--enable-static-pie
+apk add --no-cache make gcc gawk bison linux-headers libc-dev
+../configure --prefix=/usr\--disable-profile --enable-add-ons\--libexecdir=/usr/lib --with-headers=/usr/include\--without-cvs --enable-static-pie
+cat >/etc/ld.so.conf << "EOF" # Begin/etc/ld.so.conf/usr/local/lib/opt/lib/usr/lib/usr/lib64/usr/libexeС# End/etc/ld.so.conf EOF
 make
 make install
