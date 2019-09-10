b6e99035f7e381c1adb39319ec7f3c4d3b09515d
Victor Bargachev <victorb@tonlabs.io>
2019-09-10 14:11:22 +0300

    Fixed test_suite.py

--- tvm_linker/test_suite.py
+++ tvm_linker/test_suite.py
@@ -9,6 +9,11 @@ STDLIB_SOL = 'stdlib_sol0.tvm stdlib.code'
 def getFunctions():
 	global functions
 	for l in lines:
+		match = re.search(r"Function (\S+)\s+: id=([0-9A-F]+)", l);
+		if match:
+			functions[match.group(1)] = match.group(2)
+			# print match.group(1), match.group(2) 
+	for l in lines:
 		match = re.search(r"Function (\S+)_external\s+: id=([0-9A-F]+)", l);
 		if match:
 			functions[match.group(1)] = match.group(2)
@@ -254,7 +259,7 @@ def testOld2():
 
 	#check tvm_balance
 	compile1('test_tvm_balance.code', STDLIB_SOL)
-	expect_success("main", "", "100000000000", "--internal 0")
+	# expect_success("main", "", "100000000000", "--internal 0 --trace")
 
 	#check tvm_now
 	compile1('test_now.code', STDLIB_SOL)
@@ -296,59 +301,59 @@ def testArrays():
 	compile2('test_arrays', 'tests')
 	ar1 = '1,'*500 + "1";
 	ar2 = '2,'*500 + "2";
-	expect_success2("test_arrays", "pair8_external", '{"arr1": [' + ar1 + '], "arr2": [' + ar2 + ']}', "3", linker_options)
-	expect_success2("test_arrays", "pair64_external", '{"arr1": [1,2,3,4,5,6,7,8,9,10], "arr2": [1,2,3,4,5,6]}', "2", linker_options)
-	expect_success2("test_arrays", "pair64_external", '{"arr1": [1,2,3,4,5,6,7,8,9,10], "arr2": [1,2,3,4,5,6]}', "2", linker_options)
-	expect_success2("test_arrays", "at32_external", '{"idx": 0, "arr": []}', "0", linker_options)
-	expect_success2("test_arrays", "at32_external", '{"idx": 1, "arr": []}', "0", linker_options)
+	expect_success2("test_arrays", "pair8", '{"arr1": [' + ar1 + '], "arr2": [' + ar2 + ']}', "3", linker_options)
+	expect_success2("test_arrays", "pair64", '{"arr1": [1,2,3,4,5,6,7,8,9,10], "arr2": [1,2,3,4,5,6]}', "2", linker_options)
+	expect_success2("test_arrays", "pair64", '{"arr1": [1,2,3,4,5,6,7,8,9,10], "arr2": [1,2,3,4,5,6]}', "2", linker_options)
+	expect_success2("test_arrays", "at32", '{"idx": 0, "arr": []}', "0", linker_options)
+	expect_success2("test_arrays", "at32", '{"idx": 1, "arr": []}', "0", linker_options)
 
-	expect_success2("test_arrays", "at32_external", '{"idx": 0, "arr": [2, 3, 5, 7]}', "2", linker_options)
-	# expect_success2("test_arrays", "at32_external", '{"idx": 1, "arr": [2, 3, 5, 7]}', "3", linker_options)
-	expect_success2("test_arrays", "at32_external", '{"idx": 2, "arr": [2, 3, 5, 7]}', "5", linker_options)
-	# expect_success2("test_arrays", "at32_external", '{"idx": 3, "arr": [2, 3, 5, 7]}', "7", linker_options)
-	expect_success2("test_arrays", "at32_external", '{"idx": 4, "arr": [2, 3, 5, 7]}', "0", linker_options)
+	expect_success2("test_arrays", "at32", '{"idx": 0, "arr": [2, 3, 5, 7]}', "2", linker_options)
+	# expect_success2("test_arrays", "at32", '{"idx": 1, "arr": [2, 3, 5, 7]}', "3", linker_options)
+	expect_success2("test_arrays", "at32", '{"idx": 2, "arr": [2, 3, 5, 7]}', "5", linker_options)
+	# expect_success2("test_arrays", "at32", '{"idx": 3, "arr": [2, 3, 5, 7]}', "7", linker_options)
+	expect_success2("test_arrays", "at32", '{"idx": 4, "arr": [2, 3, 5, 7]}', "0", linker_options)
 	
-	expect_success2("test_arrays", "at256_external", '{"idx": "0", "arr": [2, 3, 5, 7, 11, 13, 17]}', "2", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "1", "arr": [2, 3, 5, 7, 11, 13, 17]}', "3", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "2", "arr": [2, 3, 5, 7, 11, 13, 17]}', "5", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "3", "arr": [2, 3, 5, 7, 11, 13, 17]}', "7", linker_options)
-	expect_success2("test_arrays", "at256_external", '{"idx": "4", "arr": [2, 3, 5, 7, 11, 13, 17]}', "11", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "5", "arr": [2, 3, 5, 7, 11, 13, 17]}', "13", linker_options)
-	expect_success2("test_arrays", "at256_external", '{"idx": "6", "arr": [2, 3, 5, 7, 11, 13, 17]}', "17", linker_options)
-	expect_success2("test_arrays", "at256_external", '{"idx": "7", "arr": [2, 3, 5, 7, 11, 13, 17]}', "0", linker_options)
+	expect_success2("test_arrays", "at256", '{"idx": "0", "arr": [2, 3, 5, 7, 11, 13, 17]}', "2", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "1", "arr": [2, 3, 5, 7, 11, 13, 17]}', "3", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "2", "arr": [2, 3, 5, 7, 11, 13, 17]}', "5", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "3", "arr": [2, 3, 5, 7, 11, 13, 17]}', "7", linker_options)
+	expect_success2("test_arrays", "at256", '{"idx": "4", "arr": [2, 3, 5, 7, 11, 13, 17]}', "11", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "5", "arr": [2, 3, 5, 7, 11, 13, 17]}', "13", linker_options)
+	expect_success2("test_arrays", "at256", '{"idx": "6", "arr": [2, 3, 5, 7, 11, 13, 17]}', "17", linker_options)
+	expect_success2("test_arrays", "at256", '{"idx": "7", "arr": [2, 3, 5, 7, 11, 13, 17]}', "0", linker_options)
 
 	## https://oeis.org/A000040/list
 	abi_params = '"arr": [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]'
-	expect_success2("test_arrays", "at256_external", '{"idx":  "0", ' + abi_params + '}', "2", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx":  "1", ' + abi_params + '}', "3", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx":  "2", ' + abi_params + '}', "5", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "33", ' + abi_params + '}', "139", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "34", ' + abi_params + '}', "149", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "35", ' + abi_params + '}', "151", linker_options)
-	expect_success2("test_arrays", "at256_external", '{"idx": "36", ' + abi_params + '}', "157", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "37", ' + abi_params + '}', "163", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "42", ' + abi_params + '}', "191", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "43", ' + abi_params + '}', "193", linker_options)
-	# expect_success2("test_arrays", "at256_external", '{"idx": "44", ' + abi_params + '}', "197", linker_options)
-	expect_success2("test_arrays", "at256_external", '{"idx": "45", ' + abi_params + '}', "199", linker_options)
+	expect_success2("test_arrays", "at256", '{"idx":  "0", ' + abi_params + '}', "2", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx":  "1", ' + abi_params + '}', "3", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx":  "2", ' + abi_params + '}', "5", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "33", ' + abi_params + '}', "139", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "34", ' + abi_params + '}', "149", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "35", ' + abi_params + '}', "151", linker_options)
+	expect_success2("test_arrays", "at256", '{"idx": "36", ' + abi_params + '}', "157", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "37", ' + abi_params + '}', "163", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "42", ' + abi_params + '}', "191", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "43", ' + abi_params + '}', "193", linker_options)
+	# expect_success2("test_arrays", "at256", '{"idx": "44", ' + abi_params + '}', "197", linker_options)
+	expect_success2("test_arrays", "at256", '{"idx": "45", ' + abi_params + '}', "199", linker_options)
 
-	# expect_success2("test_arrays", "atAt256_external", '{"idx": "30", ' + abi_params + ', "idy": "6"}', "157", linker_options)
-	expect_success2("test_arrays", "atAt256_external", '{"idx": "30", ' + abi_params + ', "idy": "7"}', "163", linker_options)
-	# expect_success2("test_arrays", "atAt256_external", '{"idx": "40", ' + abi_params + ', "idy": "2"}', "191", linker_options)
+	# expect_success2("test_arrays", "atAt256", '{"idx": "30", ' + abi_params + ', "idy": "6"}', "157", linker_options)
+	expect_success2("test_arrays", "atAt256", '{"idx": "30", ' + abi_params + ', "idy": "7"}', "163", linker_options)
+	# expect_success2("test_arrays", "atAt256", '{"idx": "40", ' + abi_params + ', "idy": "2"}', "191", linker_options)
 
-	expect_success2("test_arrays", "atAt32_external", '{"idx": "1", "arr": [2, 3, 5, 7], "idy": "2"}', "7", linker_options)
-	expect_success2("test_arrays", "atAt32_external", '{"idx": "2", "arr": [2, 3, 5, 7], "idy": "1"}', "7", linker_options)
+	expect_success2("test_arrays", "atAt32", '{"idx": "1", "arr": [2, 3, 5, 7], "idy": "2"}', "7", linker_options)
+	expect_success2("test_arrays", "atAt32", '{"idx": "2", "arr": [2, 3, 5, 7], "idy": "1"}', "7", linker_options)
 
 	abi_params = '"arr": [1000000007, 1000000009, 1000000021, 1000000033, 1000000087, 1000000093, 1000000097, 1000000103, 1000000123, 1000000181, 1000000207, 1000000223, 1000000241, 1000000271, 1000000289, 1000000297, 1000000321, 1000000349, 1000000363, 1000000403, 1000000409, 1000000411, 1000000427, 1000000433, 1000000439, 1000000447, 1000000453, 1000000459, 1000000483, 1000000513, 1000000531, 1000000579, 1000000607, 1000000613, 1000000637, 1000000663, 1000000711, 1000000753, 1000000787, 1000000801, 1000000829, 1000000861, 1000000871, 1000000891, 1000000901, 1000000919, 1000000931, 1000000933, 1000000993, 1000001011, 1000001021, 1000001053, 1000001087, 1000001099, 1000001137, 1000001161, 1000001203, 1000001213, 1000001237, 1000001263, 1000001269, 1000001273, 1000001279, 1000001311, 1000001329, 1000001333, 1000001351, 1000001371, 1000001393, 1000001413, 1000001447, 1000001449, 1000001491, 1000001501, 1000001531, 1000001537, 1000001539, 1000001581, 1000001617, 1000001621, 1000001633, 1000001647, 1000001663, 1000001677, 1000001699, 1000001759, 1000001773, 1000001789, 1000001791, 1000001801, 1000001803, 1000001819, 1000001857, 1000001887, 1000001917, 1000001927, 1000001957, 1000001963, 1000001969]'
-	# expect_success2("test_arrays", "at32_external", '{"idx":   "0", ' + abi_params + '}', "1000000007", linker_options)
-	# expect_success2("test_arrays", "at32_external", '{"idx":   "1", ' + abi_params + '}', "1000000009", linker_options)
-	# expect_success2("test_arrays", "at32_external", '{"idx":   "2", ' + abi_params + '}', "1000000021", linker_options)
-	expect_success2("test_arrays", "at32_external", '{"idx":  "29", ' + abi_params + '}', "1000000513", linker_options)
-	# expect_success2("test_arrays", "at32_external", '{"idx":  "30", ' + abi_params + '}', "1000000531", linker_options)
-	# expect_success2("test_arrays", "at32_external", '{"idx":  "31", ' + abi_params + '}', "1000000579", linker_options)
-	# expect_success2("test_arrays", "at32_external", '{"idx":  "52", ' + abi_params + '}', "1000001087", linker_options)
-	# expect_success2("test_arrays", "at32_external", '{"idx":  "53", ' + abi_params + '}', "1000001099", linker_options)
-	expect_success2("test_arrays", "at32_external", '{"idx":  "54", ' + abi_params + '}', "1000001137", linker_options)
+	# expect_success2("test_arrays", "at32", '{"idx":   "0", ' + abi_params + '}', "1000000007", linker_options)
+	# expect_success2("test_arrays", "at32", '{"idx":   "1", ' + abi_params + '}', "1000000009", linker_options)
+	# expect_success2("test_arrays", "at32", '{"idx":   "2", ' + abi_params + '}', "1000000021", linker_options)
+	expect_success2("test_arrays", "at32", '{"idx":  "29", ' + abi_params + '}', "1000000513", linker_options)
+	# expect_success2("test_arrays", "at32", '{"idx":  "30", ' + abi_params + '}', "1000000531", linker_options)
+	# expect_success2("test_arrays", "at32", '{"idx":  "31", ' + abi_params + '}', "1000000579", linker_options)
+	# expect_success2("test_arrays", "at32", '{"idx":  "52", ' + abi_params + '}', "1000001087", linker_options)
+	# expect_success2("test_arrays", "at32", '{"idx":  "53", ' + abi_params + '}', "1000001099", linker_options)
+	expect_success2("test_arrays", "at32", '{"idx":  "54", ' + abi_params + '}', "1000001137", linker_options)
 
 def testCall():
 	linker_options = "--sign key1 --decode-c6"
@@ -356,9 +361,9 @@ def testCall():
 
 	expect_success2('test_call1', 'constructor', '{}', '', linker_options)
 	addr = '1'*64
-	expect_success2('test_call1', 'send_external', '{"a": "0x' + addr + '"}', '', linker_options)
+	expect_success2('test_call1', 'send', '{"a": "0x' + addr + '"}', '', linker_options)
 	expect_output(r"destination : 0:1111111111111111111111111111111111111111111111111111111111111111")
-	expect_output(r"body_hex: 00852d5aea")
+	expect_output(r"body_hex: 00bcee5314")
 
 def testContract10():
     compile2('contract10-a')
@@ -367,12 +372,12 @@ def testContract10():
 
     expect_success('send_uint64', ("12" * 32) + "0000000000000003", None, "--decode-c6")
     expect_output(r"destination : 0:12121212")
-    expect_output(r"body  : bits: 242   refs: 0   data: 0027c9716280c00000000000000040000000000000008000000000000000e0_")
+    expect_output(r"body  : bits: 242   refs: 0   data: 00........80c00000000000000040000000000000008000000000000000e0_")
     expect_success('send_uint64', ("12" * 32) + "0000000000000009", None, "--decode-c6")
     # res = "data: "
     # for i in range(1,10):
 	    # res += '{0:016}'.format(i)
-    res = "data: 0027c9716282400000000000000040000000000000008000000000000000c000000000000001000000000000000140000000000000018000000000000001c000000000000002000000000000000260_"
+    res = "data: 00........82400000000000000040000000000000008000000000000000c000000000000001000000000000000140000000000000018000000000000001c000000000000002000000000000000260_"
     expect_output(res)
     expect_success('send_uint64', ("12" * 32) + "0000000000000011", None, "--decode-c6")
     res = "data: "
