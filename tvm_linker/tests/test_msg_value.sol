f3907e1b2b7ecee447d9e14c815c0a144b73a609
Nikita.m <nikita.m@tonlabs.io>
2019-11-08 16:56:38 +0300

    removed unused test contracts

--- tvm_linker/tests/test_msg_value.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-contract TestMsgValue {
-	
-	function main() payable public returns (uint256) {
-		return msg.value;
-	}
-	
-}
--- tvm_linker/tests/test_now_variable.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.0;
-
-contract TestNow {
-
-    function main() public payable {
-        require((now > 1566494745) && (now < 1566494745 + 60*60*24*356));
-    }
-}
