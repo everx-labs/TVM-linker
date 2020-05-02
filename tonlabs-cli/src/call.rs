47eb0a90a1b4d5209b2df7f1a1644dced39a7dc8
NikitaM <monakhovnik@gmail.com>
2020-05-02 17:16:26 +0300

    Add genphrase and genpubkey subcommands (#162)
    - Fixed printing results.
    - Refactored `call` command.
    - Version updated to 0.2.0.
    - Comments added.
    
    Co-authored-by: Nikita.m <nikita.m@tonlabs.io>

--- tonlabs-cli/src/call.rs
+++ tonlabs-cli/src/call.rs
@@ -11,13 +11,38 @@
  * See the License for the specific TON DEV software governing permissions and
  * limitations under the License.
  */
-use std::thread;
-use std::time::Duration;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
 use crate::config::Config;
+use crate::crypto::generate_keypair_from_mnemonic;
 use crate::helpers::read_keys;
-use ton_client_rs::{TonClient, TonAddress};
+use ton_client_rs::{TonClient, TonClientConfig, TonAddress, Ed25519KeyPair};
+use hex;
+
+fn load_keypair(keys: Option<String>) -> Result<Option<Ed25519KeyPair>, String> {
+    match keys {
+        Some(keys) => {
+            let words: Vec<&str> = keys.split(' ').collect();
+            if words.len() == 0 {
+                let keys = read_keys(&keys)?;
+                Ok(Some(keys))
+            } else {
+                let pair = generate_keypair_from_mnemonic(&keys)?;
+
+                let mut buffer = [0u8; 64];
+                let public_vec = hex::decode(&pair.public)
+                    .map_err(|e| format!("failed to decode public key: {}", e))?;
+                let private_vec = hex::decode(&pair.secret)
+                    .map_err(|e| format!("failed to decode private key: {}", e))?;
+                
+                buffer[..32].copy_from_slice(&private_vec);
+                buffer[32..].copy_from_slice(&public_vec);
+
+                let ed25519pair = Ed25519KeyPair::zero();
+                Ok(Some(ed25519pair.from_bytes(buffer)))
+            }
+        },
+        None => Ok(None),
+    }
+}
 
 pub fn call_contract(
     conf: Config,
@@ -25,19 +50,25 @@ pub fn call_contract(
     abi: &str,
     method: &str,
     params: &str,
-    keys_file: Option<String>,
+    keys: Option<String>,
     local: bool,
 ) -> Result<(), String> {
-    let ton = TonClient::new_with_base_url(&conf.url)
-        .map_err(|e| format!("failed to create tonclient: {}", e.to_string()))?;
+    let ton = TonClient::new(&TonClientConfig{
+        base_url: Some(conf.url.clone()),
+        message_retries_count: Some(0),
+        message_expiration_timeout: Some(20000),
+        message_expiration_timeout_grow_factor: Some(1.5),
+        message_processing_timeout: Some(20000),
+        message_processing_timeout_grow_factor: Some(1.5),
+        wait_for_timeout: None,
+        access_key: None,
+    })
+    .map_err(|e| format!("failed to create tonclient: {}", e.to_string()))?;
     
     let abi = std::fs::read_to_string(abi)
         .map_err(|e| format!("failed to read ABI file: {}", e.to_string()))?;
     
-    let keys = match keys_file {
-        Some(filename) => Some(read_keys(&filename)?),
-        None => None,
-    };
+    let keys = load_keypair(keys)?;
     
     let ton_addr = TonAddress::from_str(addr)
         .map_err(|e| format!("failed to parse address: {}", e.to_string()))?;
@@ -46,51 +77,24 @@ pub fn call_contract(
     let method_val = method.to_owned();
     let params_val = params.to_owned();
 
-    let atomic = Arc::new(AtomicBool::new(false));
-    let atomic_clone = atomic.clone();
-    
-    let thrd = if local {
+    let result = if local {
         println!("Running get-method...");
-        thread::spawn(move || {
-            let result =
-                ton.contracts.run_local(&ton_addr, None, &abi, &method_val, None, params_val.into(), None)
-                        .map_err(|e| format!("run failed: {}", e.to_string()));
-            atomic_clone.store(true, Ordering::SeqCst);
-            result
-        })
+        ton.contracts.run_local(&ton_addr, None, &abi, &method_val, None, params_val.into(), None)
+            .map_err(|e| format!("run failed: {}", e.to_string()))
     } else {
         println!("Calling method...");
-        thread::spawn(move || {
-            let result =
-                ton.contracts.run(&ton_addr, &abi, &method_val, None, params_val.into(), keys.as_ref())
-                    .map_err(|e| format!("transaction failed: {}", e.to_string()));
-            atomic_clone.store(true, Ordering::SeqCst);
-            result
-        })
+        ton.contracts.run(&ton_addr, &abi, &method_val, None, params_val.into(), keys.as_ref())
+            .map_err(|e| format!("transaction failed: {}", e.to_string()))
     };
 
-    let mut success = false;
-    for _x in 0..60 {
-        thread::sleep(Duration::from_millis(500));
-        if atomic.load(Ordering::SeqCst) {
-            success = true;
-            break;
-        }
-    }
-
-    if success {
-        let result = thrd.join().unwrap();
-        match result {
-            Ok(val) => {
-              println!("Succeded.");
-              if !val.is_null() {
-                   println!("Result = {}", val);
-              }
-            },
-            Err(estr) => { println!("Error: {}", estr); }
-        };
-    } else {
-        println!("Error: run out of time while waiting for a call result");
-    }
+    match result {
+        Ok(val) => {
+            println!("Succeded.");
+            if !val.is_null() {
+                println!("Result: {}", serde_json::to_string_pretty(&val).unwrap());
+            }
+        },
+        Err(estr) => { println!("Error: {}", estr); }
+    };
     Ok(())
 }
--- tonlabs-cli/src/genaddr.rs
+++ tonlabs-cli/src/genaddr.rs
@@ -22,7 +22,7 @@ use std::fs::OpenOptions;
 use ton_sdk;
 
 pub fn generate_address(
-    conf: Config,
+    _conf: Config,
     tvc: &str,
     abi: &str,
     wc_str: Option<&str>,
@@ -31,10 +31,10 @@ pub fn generate_address(
     initial_data: Option<&str>,
     update_tvc: bool,
 ) -> Result<(), String> {
-    let ton = TonClient::new_with_base_url(&conf.url)
+    let ton = TonClient::default()
         .map_err(|e| format!("failed to create tonclient: {}", e.to_string()))?;
-    
-    let contract = std::fs::read(tvc)
+
+        let contract = std::fs::read(tvc)
         .map_err(|e| format!("failed to read smart contract file: {}", e.to_string()))?;
 
     let abi = std::fs::read_to_string(abi)
