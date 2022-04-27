0193c54c7b7adaf92cf8491fb0e88f12ccb0a039
SilkovAlexander <slexander.s@tonlabs.io>
2022-04-27 14:56:31 +0300

    Fixed error description and added test

--- tests/test.rs
+++ tests/test.rs
@@ -43,3 +43,29 @@ fn test_compile_lib() -> Result<(), Box<dyn std::error::Error>> {
     
     Ok(())
 }
+
+#[test]
+fn test_compile_lib_error() -> Result<(), Box<dyn std::error::Error>> {
+    let contract = "tests/test_arrays.code";
+    let abi = "tests/test_arrays.abi.json";
+    let lib_path = "tests/";
+
+    let lib_var = "TVM_LINKER_LIB_PATH";
+    let prev_var =  env::var_os(lib_var);
+    env::set_var(lib_var, lib_path);
+
+    let mut cmd = Command::cargo_bin(BIN_NAME)?;
+    cmd.arg("compile")
+        .arg(contract)
+        .arg("-a")
+        .arg(abi)
+        .assert()
+        .failure()
+        .stdout(predicate::str::contains("Error: Failed to read file tests/: Is a directory (os error 21)"));
+
+    if prev_var.is_some() {
+        env::set_var(lib_var, prev_var.unwrap());
+    }
+
+    Ok(())
+}
